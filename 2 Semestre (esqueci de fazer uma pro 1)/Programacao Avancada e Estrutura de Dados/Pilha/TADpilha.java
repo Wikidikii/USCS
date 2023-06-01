ea912d74023d03d7b9b7c0b2711ecc5031c35ac8
Kawê Almeida <68926941+Wikidikii@users.noreply.github.com>
2023-05-31 23:48:46 -0300

    Update TADpilha.java

--- 2 Semestre (esqueci de fazer uma pro 1)/Programacao Avancada e Estrutura de Dados/Pilha/TADpilha.java
+++ 2 Semestre (esqueci de fazer uma pro 1)/Programacao Avancada e Estrutura de Dados/Pilha/TADpilha.java
@@ -1,1 +1,55 @@
-
+public class TADPilha {
+    
+    //Atributos
+    int dados[];
+    int topo;
+    int capacidade;
+    
+    //Construtor
+    public TADPilha (int c) {
+        capacidade = c;
+        topo = -1;
+        dados = new int [capacidade];
+    }
+    
+    //Metodos
+    public boolean empilha(int x) {
+        if (!cheia()) {
+            System.out.print("CHEIO");
+            topo++;
+            dados[topo] = x;
+            return true;
+            
+        }
+        return false;
+        
+        if (vazia()) {
+            System.out.print("VAZIO");
+        }
+        else return false;
+    }
+    
+    public void MostraAoContrario() {
+        for (int i = topo; i >= 0; i--) {
+            System.out.print(dados[i]);
+        }
+    }
+    
+    public void Mostra() {
+        for (int i = 0; i <= topo; i++) {
+            System.out.print(dados[i]);
+        }
+    }
+    
+    public boolean cheia() {
+        return (topo == capacidade - 1);
+    }
+    
+    public boolean vazia() {
+        return (topo == -1);
+    }
+    
+    public int conta() {
+        return topo+1;
+    }
+}
